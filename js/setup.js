2c441a0e50a0156219c8776c6fcdfcd71a026ed6
rasmniel <rasmus@nobrainer.dk>
2021-04-26 14:21:28 +0200

    Some old changes...

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight);
 camera.position.z = 100;
 
 
@@ -16,4 +16,3 @@ function render(object) {
     // May call this at any time to render. Should call initially.
     renderer.render(scene, camera);
 }
-
